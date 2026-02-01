82d073f9811bdbf77c028a91b65b905362487a92
Chris Overcash <covercash2@gmail.com>
2026-02-01 12:41:22 -0600

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 /// A "Hello, world!" program.
 /// More comments.
 fn main() {
-    print_hello();
-    print_goodbye();
+    print("Hello, world!");
+    print("Goodbye, world!");
 }
 
-fn print_hello() {
-    println!("Hello, world!");
-}
-
-fn print_goodbye() {
-    println!("Goodbye, world!");
+/// A function that prints a message.
+fn print(m: &str) {
+    println!("{m}");
 }
